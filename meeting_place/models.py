ae7a80f3c2bff342eced5643f4053eeba1dabc0b
Warren Zhang <warrenzhang@Warrens-MacBook-Pro.local>
2017-06-12 00:06:51 +0800

    chaning a small bug on lcs

--- meeting_place/models.py
+++ meeting_place/models.py
@@ -18,13 +18,13 @@ def lcs(S,T,word):
     n = len(T)
     counter = [[0]*(n+1) for x in range(m+1)]
     longest = 0
+    lcs_set = ""
     for i in range(m):
         for j in range(n):
             if S[i] == T[j]:
                 c = counter[i][j] + 1
                 counter[i+1][j+1] = c
                 if c > longest:
-                    lcs_set = set()
                     longest = c
                     lcs_set = S[i-c+1:i+1]
     return lcs_set
